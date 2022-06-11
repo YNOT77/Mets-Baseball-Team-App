f81bcc5395368a8d097e36a3d3bfbc0150fd6563
Anthony Moran <anthony771998@gmail.com>
2022-06-10 20:29:06 -0400

    Added updated css and html files

--- main.js
+++ main.js
@@ -2,5 +2,22 @@ document.querySelector('button').addEventListener('click', apiRequest);
 
 
 async function apiRequest(){
+    const playerName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://mets-baseball-team-api.herokuapp.com/api/${playerName}`)
+        const data = await response.json()
+        console.log(data)
+        document.querySelector('.right-col').style.visibility = "visible"
+        document.getElementById("playerName").innerText = `${data['firstName']} ${data['lastName']}`
+        document.getElementById("playerImage").src = data['image']
 
+        document.getElementById("jerseyNumber").innerText = data['jerseyNumber']
+        document.getElementById("position").innerText = data['position']
+        document.getElementById("bats").innerText = data['bats']
+        document.getElementById("throws").innerText = data['throws']
+        document.getElementById("age").innerText = data['age']
+        document.getElementById("nickname").innerText = data['nickname']
+    }catch(error){
+        console.log(error)
+    }
 }
